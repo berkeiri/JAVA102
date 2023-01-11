f70dfff3f716488a9a279bcc707d1c688f67eec1
berkeiri <bquass07@gmail.com>
2023-01-11 22:39:31 +0300

    firs commit

---
+++ Colection/Match/Main.java
@@ -0,0 +1,20 @@
+package Colection.Match;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Team> teams= new ArrayList<>();
+        teams.add(new Team("Galatasaray"));
+        teams.add(new Team("Fenerbahçe"));
+        teams.add(new Team("Trabzonspor"));
+        teams.add(new Team("Beşiktaş"));
+        teams.add(new Team("Bursaspor"));
+        teams.add(new Team("Başakşehir"));
+
+        ArrayList<Fixture> fixtureArrayList = Fixture.FixtureGenerator(teams);
+        Fixture.print(fixtureArrayList);
+
+
+    }
+}
